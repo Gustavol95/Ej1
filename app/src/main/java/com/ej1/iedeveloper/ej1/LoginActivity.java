67d9e3b6361c77f289a7c191c9b9804a003efe34
IE Developer <iedeveloper@Mac-mini-de-IE.local>
2016-12-23 13:01:20 -0700

    - Fin de día, reactive jalando bien, mejorar logica y Objeto de control

--- app/src/main/java/com/ej1/iedeveloper/ej1/LoginActivity.java
+++ app/src/main/java/com/ej1/iedeveloper/ej1/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -13,13 +14,9 @@ import android.support.v7.widget.AppCompatEditText;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ej1.iedeveloper.ej1.interfaces.ServicioWeb;
@@ -38,13 +35,18 @@ import rx.Observable;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.functions.Func2;
 import rx.schedulers.Schedulers;
+import rx.subjects.PublishSubject;
 
-public class LoginActivity extends AppCompatActivity implements View.OnFocusChangeListener {
+public class LoginActivity extends AppCompatActivity  {
     public static String TAG = "ActivityLogin";
     private static String URL = "http://lab.ie-soluciones.com/tapanosa/apiv2/movil/";
     private Observable<LoginResponse> observableLogin;
     private Subscription subscription;
+    private Subscription subBoton;
     private ImageView imageViewLogo;
     private AppCompatButton buttonIniciarSesion;
     private TextInputLayout textInputEditTextUsuario;
@@ -54,8 +56,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnFocusChan
     private ServicioWeb servicioWeb;
     private LoginResponse loginActual;
     private ProgressBar progressBar;
-    private boolean emailOk = false;
-    private boolean passwordOk = false;
+    private boolean emailVisited=false;
+    private boolean emailDirty=false;
+    private boolean emailCorrecto=false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -82,30 +85,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnFocusChan
         observableLogin = getLoginResponse()
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io());
-        editTextUsuario.setOnFocusChangeListener(this);
-        editTextContrasena.setOnFocusChangeListener(this);
-        editTextContrasena.setImeOptions(EditorInfo.IME_ACTION_DONE);
-        editTextContrasena.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                switch (actionId) {
-
-                    case EditorInfo.IME_ACTION_DONE:
-                        View view = LoginActivity.this.getCurrentFocus();
-                        if (view != null) {
-                            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-                        }
-                        editTextContrasena.clearFocus();
-                        Log.i(TAG, "Enter");
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-        });
-
+        setRxInputValidations();
     }
+
 
 
     public void setRetrofit() {
@@ -122,9 +104,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnFocusChan
             @Override
             public void onCompleted() {
                 Log.i(TAG, "onCompleted");
-                Intent i=new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(i);
-                finish();
+
             }
 
             @Override
@@ -141,19 +121,116 @@ public class LoginActivity extends AppCompatActivity implements View.OnFocusChan
         });
     }
 
-    @Override
-    public void onBackPressed() {
+    public void setRxInputValidations(){
+        Observable<Boolean> observableEmail=getTextWatcherObservable(editTextUsuario)
+                .map(new Func1<String, Boolean>() {
+                    @Override
+                    public Boolean call(String s) {
+                        Log.i(TAG, "observable Email "+!isValidEmail(s)+" "+emailVisited);
+                        emailDirty=true;
+                        emailCorrecto=isValidEmail(s);
+                        if(emailVisited && !emailCorrecto){
+                            textInputEditTextUsuario.setErrorEnabled(true);
+                            textInputEditTextUsuario.setError("Email inválido");
+                        }
+                        else
+                            textInputEditTextUsuario.setErrorEnabled(false);
+                        return emailCorrecto;
+                    }
+                });
 
-        Log.i(TAG, "Tiene focus contraseña");
-        View view = LoginActivity.this.getCurrentFocus();
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-            editTextContrasena.clearFocus();
+        observableEmail.subscribe(new Action1<Boolean>() {
+            @Override
+            public void call(Boolean aBoolean) {
 
-        }
+            }
+        });
 
-        super.onBackPressed();
+       getOnFocusChangeObservable(editTextUsuario)
+                .subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean hasFocus) {
+                        if(!hasFocus )
+                            emailVisited=true;
+
+                        if(emailVisited && !emailCorrecto){
+                            textInputEditTextUsuario.setErrorEnabled(true);
+                            textInputEditTextUsuario.setError("Email inválido");
+                        }
+                        else
+                            textInputEditTextUsuario.setErrorEnabled(false);
+
+                        Log.i(TAG,"hasFocus "+hasFocus);
+                        Log.i(TAG, "dirty "+emailDirty);
+                        Log.i(TAG, "EmailVisited "+emailVisited);
+                    }
+                });
+
+        Observable<Boolean> observablePassword=getTextWatcherObservable(editTextContrasena)
+                .map(new Func1<String, Boolean>() {
+                    @Override
+                    public Boolean call(String s) {
+                        return !s.trim().isEmpty();
+                    }
+                });
+         subBoton=Observable.combineLatest(observableEmail, observablePassword, new Func2<Boolean, Boolean, Boolean>() {
+            @Override
+            public Boolean call(Boolean emailValido, Boolean passwordValido) {
+                Log.i(TAG,"email : "+emailValido);
+                Log.i(TAG,"pass : "+passwordValido);
+                //Mostrar u ocultar errores en el input
+                if(!emailValido){
+                    textInputEditTextUsuario.setErrorEnabled(true);
+                    textInputEditTextUsuario.setError("Email inválido");
+                }
+                else
+                    textInputEditTextUsuario.setErrorEnabled(false);
+
+
+                return emailValido && passwordValido;
+            }
+        })
+                .subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean todoValido) {
+                        if(todoValido)
+                            activarBoton();
+                        else
+                            desactivarBoton();
+                    }
+                });
+
+
+    }
+
+    public Observable<String> getTextWatcherObservable(@NonNull final AppCompatEditText editText) {
+
+        final PublishSubject<String> subject = PublishSubject.create();
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                subject.onNext(s.toString());
+            }
+        });
+
+        return subject;
+    }
+
+    public Observable<Boolean> getOnFocusChangeObservable(final View view){
+        final PublishSubject<Boolean> subject = PublishSubject.create();
+        view.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                subject.onNext(hasFocus);
+            }
+        });
+        return subject;
     }
 
     public Observable<LoginResponse> getLoginResponse() {
@@ -179,12 +256,18 @@ public class LoginActivity extends AppCompatActivity implements View.OnFocusChan
             public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
                 Log.i(TAG, response.message());
                 if (response.isSuccessful() && response.code() == 200) {
+                    try{
                     SharedPreferences pref = getApplicationContext().getSharedPreferences(getString(R.string.shared_prefs), 0); // 0 - for private mode
                     SharedPreferences.Editor editor = pref.edit();
                     editor.putString("token", response.body().getToken());
                     editor.apply();
                     Log.i(TAG, "Entró token: " + response.body().getToken());
-                    Toast.makeText(LoginActivity.this, "Se registró token", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Se registró token", Toast.LENGTH_SHORT).show();}
+                    finally{
+                        Intent i=new Intent(LoginActivity.this,MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
                 } else if (response.code() != 200) {
                     Toast.makeText(LoginActivity.this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show();
                 }
@@ -223,40 +306,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnFocusChan
         this.loginActual = loginActual;
     }
 
-    @Override
-    public void onFocusChange(View v, boolean hasFocus) {
-        if (!hasFocus) {
-            switch (v.getId()) {
-                case R.id.editText_usuario:
-                    if (!isValidEmail(editTextUsuario.getText().toString().trim())) {
-                        textInputEditTextUsuario.setErrorEnabled(true);
-                        textInputEditTextUsuario.setError("E-mail incorrecto.");
-                        emailOk = false;
-
-                    } else {
-                        textInputEditTextUsuario.setErrorEnabled(false);
-                        emailOk = true;
-                    }
-                    break;
-                case R.id.editText_contrasena:
-                    if (editTextContrasena.getText().toString().isEmpty()) {
-                        textInputEditTextContrasena.setErrorEnabled(true);
-                        textInputEditTextContrasena.setError("No puede ser vacío");
-                        passwordOk = false;
-
-                    } else {
-                        passwordOk = true;
-                        textInputEditTextContrasena.setErrorEnabled(false);
-
-                    }
-                    break;
-            }
-            validarBoton();
-        }
-
-
-    }
-
     public final boolean isValidEmail(String target) {
         if (target == null) {
             return false;
@@ -266,18 +315,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnFocusChan
         }
     }
 
-    public void validarBoton() {
-        Log.i(TAG, "validar Boton " + emailOk + passwordOk);
-        if (emailOk && passwordOk) {
-            buttonIniciarSesion.setEnabled(true);
-            buttonIniciarSesion.setTextColor(Color.WHITE);
-            buttonIniciarSesion.setBackgroundResource(R.color.azulDesbloqueado);
-        } else {
-            buttonIniciarSesion.setEnabled(false);
-            buttonIniciarSesion.setTextColor(getResources().getColor(R.color.grisBloqueado));
-            buttonIniciarSesion.setBackgroundResource(R.color.grisBloqueadoFondo);
-        }
+
+    public void activarBoton(){
+        buttonIniciarSesion.setEnabled(true);
+        buttonIniciarSesion.setTextColor(Color.WHITE);
+        buttonIniciarSesion.setBackgroundResource(R.color.azulDesbloqueado);
     }
 
+    public void desactivarBoton(){
+        buttonIniciarSesion.setEnabled(false);
+        buttonIniciarSesion.setTextColor(getResources().getColor(R.color.grisBloqueado));
+        buttonIniciarSesion.setBackgroundResource(R.color.grisBloqueadoFondo);
+    }
 
 }
