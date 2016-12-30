1e62bcf960efd06c785941cd4e421e47aa9d24c0
IE Developer <iedeveloper@Mac-mini-de-IE.local>
2016-12-30 16:36:13 -0700

    - Borrada recycler Expandible - Implementar llamada a clientes Authorization Bearer - Guardar en Base de Datos GreenDao - Implementar vista de cronómetro con nuevo diseño. Implementar modelo cada vista del recycler agregada. - Llamar la base de datos para mostrar los clientes. - Guardar en base de datos cada inserción al recycler

--- app/src/main/java/com/ej1/iedeveloper/ej1/CronometroActivity.java
+++ app/src/main/java/com/ej1/iedeveloper/ej1/CronometroActivity.java
@@ -2,18 +2,25 @@ package com.ej1.iedeveloper.ej1;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
-import com.ej1.iedeveloper.ej1.adapter.ExpandableAdapter;
-import com.ej1.iedeveloper.ej1.model.Hijo;
-import com.ej1.iedeveloper.ej1.model.Padre;
-
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by iedeveloper on 29/12/16.
@@ -22,21 +29,71 @@ import butterknife.ButterKnife;
 public class CronometroActivity extends AppCompatActivity {
 
 
-    @BindView(R.id.recycler)  RecyclerView recyclerView;
+    @BindView(R.id.recycler)        RecyclerView recyclerView;
+    @BindView(R.id.textoCronometro) TextView textoCronometro;
+    @BindView(R.id.fab)             FloatingActionButton fab;
+
+    public static final long MILLIS_TO_MINUTES = 60000;
+    public static final long MILLS_TO_HOURS = 3600000;
+    long startTime;
+    Subscription chronometerSub;
+    String tiempoActual;
+
+    public static String TAG = "ActivityCronometro";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cronometro);
         ButterKnife.bind(this);
-        ExpandableAdapter adapter = new ExpandableAdapter(this,Arrays.asList(initializeAdapter()));
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        chronometerSub=getChonometerObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String tiempoCorriendo) {
+                        tiempoActual=tiempoCorriendo;
+                        textoCronometro.setText(tiempoActual);
+                    }
+                });
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
 
-    public Padre initializeAdapter(){
-        Hijo hijo=new Hijo("Aqui los controles");
-        Padre padre=new Padre(Arrays.asList(hijo),"1","Texto con info");
-        return padre;
+    public Observable<String> getChonometerObservable(){
+        startTime=System.currentTimeMillis();
+        return Observable.interval(15, TimeUnit.MILLISECONDS)
+                .map(new Func1<Long, String>() {
+                    @Override
+                    public String call(Long aLong) {
+                        long since = System.currentTimeMillis() - startTime;
+                        int seconds = (int) (since / 1000) % 60;
+                        int minutes = (int) ((since / (MILLIS_TO_MINUTES)) % 60);
+                        //int hours = (int) ((since / (MILLS_TO_HOURS)) % 24); //this resets to  0 after 24 hour!
+                        int hours = (int) ((since / (MILLS_TO_HOURS))); //this does not reset to 0!
+                        int millis = (int) since % 1000; //the last 3 digits of millisecs
+
+                        return String.format("%02d:%02d:%02d:%03d"
+                                , hours, minutes, seconds, millis);
+
+                    }
+                });
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (chronometerSub != null && !chronometerSub.isUnsubscribed()) {
+            chronometerSub.unsubscribe();
+        }
+    }
+
+
 }
--- app/src/main/java/com/ej1/iedeveloper/ej1/MainActivity.java
+++ app/src/main/java/com/ej1/iedeveloper/ej1/MainActivity.java
@@ -3,16 +3,9 @@ package com.ej1.iedeveloper.ej1;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.widget.TextView;
-
-import com.ej1.iedeveloper.ej1.adapter.TestRecyclerAdapter;
-import com.ej1.iedeveloper.ej1.model.RecyclerViewTestItem;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,6 +24,7 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String TAG="AcivityMain";
     @BindView(R.id.text) TextView text;
     @BindView(R.id.recycler) RecyclerView recyclerView;
     @Override
@@ -40,18 +34,75 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         SharedPreferences pref = getApplicationContext().getSharedPreferences(getString(R.string.shared_prefs), 0); // 0 - for private mode
         text.setText(pref.getString("token","No se guardó el token"));
-        StaggeredGridLayoutManager sglm=new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.VERTICAL);
-        LinearLayoutManager llm=new LinearLayoutManager(this);
-        //recyclerView.setLayoutManager(sglm);
-        recyclerView.setLayoutManager(llm);
-        TestRecyclerAdapter adapter=new TestRecyclerAdapter(initializeAdapter(),this);
-        recyclerView.setAdapter(adapter);
-    }
-    public List<RecyclerViewTestItem> initializeAdapter(){
-        List<RecyclerViewTestItem> items=new ArrayList<>();
-        for(int i=0;i<100;i++)
-        items.add(new RecyclerViewTestItem("Item "+(i+1)));
+        Log.d(TAG,pref.getString("token","No se guardó el token"));
 
-        return items;
     }
+
 }
+/*
+[
+        {
+        "id_cliente": 1,
+        "nombre_cte": "MARIA ISABEL DE LA TORRE OLIVAS",
+        "observaciones": null,
+        "fecha_alta": "2016-09-28",
+        "zona_id": 13,
+        "ruta_id": 3,
+        "created_at": "2016-09-28 06:23:40",
+        "updated_at": "2016-11-28 16:01:13",
+        "deleted_at": null,
+        "proratear_abono": "0",
+        "desc_ruta": "Ruta Pruebas",
+        "desc_zona": "ZONA PALMITO",
+        "domicilios": [
+        {
+        "id": 11,
+        "id_cliente": 1,
+        "calle": "calle no se",
+        "num_ext": "5432",
+        "num_int": null,
+        "cp": "32767",
+        "id_zona": 2,
+        "id_ruta": 3,
+        "id_colonia": 16531,
+        "contacto_cliente_domicilio": null,
+        "observaciones_cliente_dom": null,
+        "lat": 24.816372,
+        "lng": -107.376886,
+        "created_at": "2016-11-17 11:22:49",
+        "updated_at": "2016-11-28 16:01:13",
+        "deleted_at": null
+        }
+        ]
+        },
+        {
+        "id_cliente": 107,
+        "nombre_cte": "PRUEBAS IE",
+        "observaciones": null,
+        "fecha_alta": "2016-09-30",
+        "zona_id": 2,
+        "ruta_id": 3,
+        "created_at": "2016-09-30 07:07:25",
+        "updated_at": "2016-10-05 17:11:50",
+        "deleted_at": null,
+        "proratear_abono": "0",
+        "desc_ruta": "Ruta Pruebas",
+        "desc_zona": "ZONA QUINTAS",
+        "domicilios": []
+        },
+        {
+        "id_cliente": 108,
+        "nombre_cte": "PRUEBAS IE RUTAS",
+        "observaciones": null,
+        "fecha_alta": "2016-10-03",
+        "zona_id": 2,
+        "ruta_id": 3,
+        "created_at": "2016-10-03 10:49:31",
+        "updated_at": "2016-10-13 22:35:55",
+        "deleted_at": null,
+        "proratear_abono": "0",
+        "desc_ruta": "Ruta Pruebas",
+        "desc_zona": "ZONA QUINTAS",
+        "domicilios": []
+        }
+        ]*/
